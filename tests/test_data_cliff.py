e34cc8b1159ef7ff33c1eca8a708203021738e5e
ruizdesotto <miguel.ruizdesotto@gmail.com>
2023-02-21 21:55:59 +0100

    feat: test a ver sisii

--- tests/test_data_cliff.py
+++ tests/test_data_cliff.py
@@ -17,14 +17,17 @@ def response():
 
 def test_bare_example():
     # Given
-    from data_cliff.data_cliff import compare
+    from dvc.api import DVCFileSystem
 
-    a_rev = "b0660085fd6990399379d81d41ce2be7fcae643e"
-    b_rev = "873afcd844f6a3c77c9661135138e619ba11b11f"
-    file = "tests/dvc_test_data/dir1/file"
+    # from data_cliff.data_cliff import compare
+    # a_rev = "b0660085fd6990399379d81d41ce2be7fcae643e"
+    # b_rev = "873afcd844f6a3c77c9661135138e619ba11b11f"
+    file = "tests/dvc_test_data/dir1/file.test"
+    a_fs = DVCFileSystem(rev="HEAD")
+    a_fs.get(file, "zzzzzzzzzzzzzz")
 
     # When
-    compare(a_rev, b_rev, file)
+    # compare(a_rev, b_rev, file)
 
     # Assert
     pass
