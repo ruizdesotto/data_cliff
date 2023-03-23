7c5b3471c590374f93703e4e379b1752d0d3089a
Miguel Ruiz <62339784+ruizdesotto@users.noreply.github.com>
2023-03-23 08:31:12 +0100

    Draft: Feature/23 allow for folder diffs (#32)
    * test: added second file
    * feat: added diffs for folder

--- tests/test_data_cliff.py
+++ tests/test_data_cliff.py
@@ -17,7 +17,7 @@ def test_retrieves_file(dvc_path: Path, tmp_path: Path) -> None:
     assert out_file.is_file()
 
 
-def test_compare_staged_change(dvc_path, capsys) -> None:
+def test_compare_staged_change_on_single_file(dvc_path, capsys) -> None:
     # Given
     from dvc.api import DVCFileSystem
 
@@ -49,6 +49,40 @@ def test_compare_staged_change(dvc_path, capsys) -> None:
 
     # Finally
     DVCFileSystem(rev="HEAD").get(str(file), str(file))
+
+
+def test_compare_staged_change_on_folder(dvc_path, capsys) -> None:
+    # Given
+    from dvc.api import DVCFileSystem
+
+    from data_cliff.data_cliff import compare
+
+    folder = dvc_path / "dir"
+    _add_line_to_path(folder / "file.json")
+
+    expected_output = (
+        "cliff a/tests/test_dvc_data/local_data/dir/file.json"
+        " b/tests/test_dvc_data/local_data/dir/file.json\n"
+        "index 612420a..db800ff 100644\n"
+        "--- a/tests/test_dvc_data/local_data/dir/file.json\n"
+        "+++ b/tests/test_dvc_data/local_data/dir/file.json\n"
+        "@@ -1,3 +1,4 @@\n"
+        " {\n"
+        '\x1b[91m-    "test": "file"\x1b[00m\n'
+        '\x1b[92m+    "test": "file",\x1b[00m\n'
+        '\x1b[92m+    "new_key": "new_value"\x1b[00m\n'
+        " }\n"
+    )
+
+    # When
+    compare("HEAD", None, str(folder))
+    stdout, _ = capsys.readouterr()
+
+    # Then
+    assert stdout == expected_output
+
+    # Finally
+    DVCFileSystem(rev="HEAD").get(str(folder), str(folder), recursive=True)
 
 
 def _add_line_to_path(file: Path) -> None:
