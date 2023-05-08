90f8e39ebc2f89b0d8586b3c6575f9dba04d5128
ruizdesotto <miguel.ruizdesotto@gmail.com>
2023-05-08 19:06:47 +0200

    fix: use dev valid version

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ruizdesotto/data_cliff",
-    version="{{VERSION_PLACEHOLDER}}",
+    version="0.0.dev0",
     zip_safe=False,
 )
